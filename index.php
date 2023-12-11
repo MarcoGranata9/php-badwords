52836f740c8ea66a46d4c3ac5a7e82fd147ca92e
MarcoGranata9 <granatam9900@gmail.com>
2023-12-11 15:07:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad words</title>
+</head>
+<body>
+    <form action="">
+        <textarea name="text" id="text" cols="30" rows="10"></textarea>
+        <input type="text">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
