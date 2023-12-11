813b564b1c295fc7af581126f4ddda6c928409d5
MarcoGranata9 <granatam9900@gmail.com>
2023-12-11 16:05:13 +0100

    add: basic style

--- index.php
+++ index.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad words</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="result.php" method="GET">
-
-        <label for="text">Inserisci del testo</label>
-        <textarea name="text" id="text" cols="30" rows="10"></textarea>
-
-        <label for="word">Parola da censurare</label>
-        <input type="text" id="word" name="word">  
-
-        <button type="submit">Invia</button>
-
-    </form>
+    <div class="container">
+        <h1>Filtro parole</h1>
+        <form action="result.php" method="GET">
+    
+            <p><label for="text">Inserisci del testo</label></p>
+            <textarea name="text" id="text" cols="50" rows="5"></textarea>  
+    
+            <label for="word">Parola da censurare</label>
+            <input type="text" id="word" name="word">  
+    
+            <button type="submit">Invia</button>
+    
+        </form>
+    </div>
 </body>
 </html>
--- result.php
+++ result.php
@@ -10,14 +10,21 @@ $censored_text = str_replace($word, "***", $text);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Risultato</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Paragrafo</h2>
-    <p>Lunghezza <?php echo strlen($text); ?> caratteri</p>
-    <p><?php echo $text; ?></p>
-    
-    <p>Parola censurata: <?php echo $word; ?></p>
-    <p>Lunghezza <?php echo strlen($censored_text); ?> caratteri</p>
-    <p><?php echo $censored_text ?></p>
+    <div class="container">
+        <div class="uncensored">
+            <h2 class="margin-b">Paragrafo</h2>
+            <p class="bold margin-b">Lunghezza <?php echo strlen($text); ?> caratteri</p>
+            <p class="margin-b"><?php echo $text; ?></p>
+        </div>
+        <div class="censored">
+            <h2 class="margin-b">Paragrafo con censura</h2>
+            <p class="bold margin-b">Parola censurata: <?php echo $word; ?></p>
+            <p class="bold margin-b">Lunghezza <?php echo strlen($censored_text); ?> caratteri</p>
+            <p><?php echo $censored_text ?></p>
+        </div>
+    </div>
 </body>
 </html>
